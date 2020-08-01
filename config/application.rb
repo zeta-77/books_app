c6ba2e926290411d2e8c41ff4a3f1893815f746f
zeta-77 <bts.yuichiro@gmail.com>
2020-08-01 19:29:01 +0900

    将来のrailsアップデート時にも適応するようロードするバージョンを固定

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module BooksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 6.0　←　デフォルトのコード
+    config.load_defaults 6.0
 
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
