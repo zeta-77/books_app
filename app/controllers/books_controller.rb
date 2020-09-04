77e2e1a2e18fba5f4a1e7158c14d633ea77d23e6
zeta-77 <bts.yuichiro@gmail.com>
2020-09-05 08:34:56 +0900

    deviseを用いてログイン機能を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   CONTENTS_PER_PAGE = 5
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /books
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :books
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
