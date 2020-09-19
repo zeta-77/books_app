cd2d1e0fa6ec07848f65077881a1e39ce4b35d02
zeta-77 <bts.yuichiro@gmail.com>
2020-09-19 19:40:36 +0900

    「/users/profile」のURLで自身の登録情報が見られるよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
   def show
-    if current_user.id.to_s == params[:id].to_s then
-      @user = User.find(params[:id])
-    else
-      redirect_to root_path
-    end
+    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :books
   devise_for :users
   resources :users, only: [:show]
-
+  get 'profile', action: :show, controller: 'users'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
