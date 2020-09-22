5792ac98ae21522c372b28dc43cd39bd9f31abb7
zeta-77 <bts.yuichiro@gmail.com>
2020-09-23 08:35:17 +0900

    「/profile」でユーザ情報が閲覧できるよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
   devise_for :users
-  resources :users, only: [:show]
-  get 'profile', action: :show, controller: 'users'
-  
+  resource :profile, only: [:show], controller: 'users'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
