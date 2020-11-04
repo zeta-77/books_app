6ea36cd4e29784c2bf09dfe8261da5b2e299046b
zeta-77 <bts.yuichiro@gmail.com>
2020-11-04 19:01:50 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks",
     registrations: "users/registrations"
   }
-  resource :profile, only: [:show], controller: 'users'
+  resource :profile, only: [:show]
+  resources :users, only: [:index, :show]
+  resources :follow_users, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
