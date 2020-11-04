7d6cfe929bd2f599358b17dc07fea5de2a326058
zeta-77 <bts.yuichiro@gmail.com>
2020-11-04 18:34:07 +0900

    follow_usersコントローラーの実装

---
+++ app/controllers/follow_users_controller.rb
@@ -0,0 +1,19 @@
+class FollowUsersController < ApplicationController
+
+  def create
+    following = current_user.follow(followed_user(params[:followed_user_id]))
+    following.save
+    redirect_to users_path
+  end
+
+  def destroy
+    current_user.unfollow(followed_user(params[:id]))
+    redirect_to users_path
+  end
+
+  private
+  def followed_user(id)
+    User.find(id)
+  end
+end  
+
