e004416334ac2e09f0be3221f400544787fc7286
zeta-77 <bts.yuichiro@gmail.com>
2020-11-26 21:37:55 +0900

    不要なcontrollerのテストを削除

--- test/controllers/follow_users_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FollowUsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/user_followings_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UserFollowingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get user_followings_index_url
-    assert_response :success
-  end
-
-end
