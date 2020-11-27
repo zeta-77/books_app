a52d3ebbb9798609fca152f73132666def2cfb25
zeta-77 <bts.yuichiro@gmail.com>
2020-11-27 09:58:12 +0900

    if文の「空白でなければ」という条件における不要な記載を削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    if params[:content] != ""
+    if params[:content]
       comment = Comment.new(params.permit(:user_id, :commentable_id, :commentable_type, :content))
       comment.save
       redirect_back(fallback_location: root_path)
