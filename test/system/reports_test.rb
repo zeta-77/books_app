d0f4d47f5b2e4a60c0dbb404077eeb510f8cf929
zeta-77 <bts.yuichiro@gmail.com>
2020-12-10 19:28:39 +0900

    reportのシステムテスト実装

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -2,44 +2,58 @@ require "application_system_test_case"
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = reports(:one)
+    FactoryBot.create(:report)
+    sign_in_as(FactoryBot.create(:user))
   end
 
-  test "visiting the index" do
+  test 'Reportの一覧表示' do
     visit reports_url
-    assert_selector "h1", text: "Reports"
+    assert_selector 'h1', text: '日報一覧'
+    assert_text '12月10日の日報'
+    assert_text 'テストを実装した。'
   end
 
-  test "creating a Report" do
+  test 'Reportの詳細表示' do
     visit reports_url
-    click_on "New Report"
-
-    fill_in "Content", with: @report.content
-    fill_in "Title", with: @report.title
-    click_on "Create Report"
-
-    assert_text "Report was successfully created"
-    click_on "Back"
+    click_on '詳細'
+    assert_selector 'h1', text: 'レポート詳細'
+    assert_selector 'h1', text: 'コメント'
+    assert_text '12月10日の日報'
+    assert_text 'テストを実装した。'
   end
 
-  test "updating a Report" do
+  test 'Reportの新規登録' do
     visit reports_url
-    click_on "Edit", match: :first
+    click_on '日報の新規作成'
+    fill_in 'タイトル', with: '2021年の抱負'
+    fill_in '内容', with: '肉体改造'
+    click_on '登録する'
 
-    fill_in "Content", with: @report.content
-    fill_in "Title", with: @report.title
-    click_on "Update Report"
-
-    assert_text "Report was successfully updated"
-    click_on "Back"
+    assert_selector 'p#notice', text: '日報が登録されました。'
+    assert_text '2021年の抱負'
+    assert_text '肉体改造'
   end
 
-  test "destroying a Report" do
+  test 'Reportの編集' do
     visit reports_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
+    click_on '編集'
+    fill_in 'タイトル', with: '12月11日の報告'
+    fill_in '内容', with: 'Railsの課題終了'
+    click_on '更新する'
+
+    assert_selector 'p#notice', text: '日報が更新されました。'
+    assert_selector 'h1', text: 'レポート詳細'
+    assert_selector 'h1', text: 'コメント'
+    assert_text '12月11日の報告'
+    assert_text 'Railsの課題終了'   
+  end
+
+  test 'Reportの削除' do
+    visit reports_url
+    accept_confirm do
+      click_on '削除'
     end
-
-    assert_text "Report was successfully destroyed"
+    assert_selector 'p#notice', text: '日報が削除されました。'
+    assert_selector 'h1', text: '日報一覧'
   end
 end
