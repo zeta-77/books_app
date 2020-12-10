45702e5f64f2469a5bdd34c910c6b5e42a5883c3
zeta-77 <bts.yuichiro@gmail.com>
2020-12-10 19:27:04 +0900

    ログイン用のメソッドの実装

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,11 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  def sign_in_as(user)
+    visit root_url
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_on 'ログイン'    
+  end
 end
