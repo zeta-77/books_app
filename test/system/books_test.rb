dd7b376dc6893f20b02ab88cde93fa2b5536ea81
zeta-77 <bts.yuichiro@gmail.com>
2020-12-10 19:27:45 +0900

    bookのシステムテスト実装

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -2,44 +2,63 @@ require "application_system_test_case"
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    FactoryBot.create(:ruby_book)
+    sign_in_as(FactoryBot.create(:user))
   end
 
-  test "visiting the index" do
-    visit books_url
-    assert_selector "h1", text: "Books"
+  test "ログイン後の初期画面の表示確認" do
+    assert_selector "h1", text: "アカウント情報"
+    assert_selector "h1", text: "フォローしているユーザの一覧"
   end
 
-  test "creating a Book" do
+  test 'Bookの一覧表示' do
     visit books_url
-    click_on "New Book"
-
-    fill_in "Memo", with: @book.memo
-    fill_in "Title", with: @book.title
-    click_on "Create Book"
-
-    assert_text "Book was successfully created"
-    click_on "Back"
+    assert_text 'Ruby超入門'
+    assert_text 'わかりやすい'
+    assert_text 'igaigaさん'
   end
 
-  test "updating a Book" do
+  test 'Bookの詳細表示' do
     visit books_url
-    click_on "Edit", match: :first
-
-    fill_in "Memo", with: @book.memo
-    fill_in "Title", with: @book.title
-    click_on "Update Book"
-
-    assert_text "Book was successfully updated"
-    click_on "Back"
+    click_on '表示'
+    assert_text 'Ruby超入門'
+    assert_text 'わかりやすい'
+    assert_text 'igaigaさん'
   end
 
-  test "destroying a Book" do
+  test 'Bookの新規登録' do
     visit books_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
+    click_on '新規登録'
+    fill_in 'タイトル', with: 'ワンピース'
+    fill_in 'メモ', with: 'Dの一族の物語'
+    fill_in '著者', with: '尾田'
+    click_on '登録する'
+
+    assert_selector 'p#notice', text: '本のデータが登録されました。'
+    assert_text 'ワンピース'
+    assert_text 'Dの一族の物語'
+    assert_text '尾田'
+  end
+
+  test 'Bookの編集' do
+    visit books_url
+    click_on '編集'
+    fill_in 'タイトル', with: 'Ruby超入門2'
+    fill_in 'メモ', with: '最新版になりました。'
+    fill_in '著者', with: '五十嵐さん2'
+    click_on '更新する'
+
+    assert_selector 'p#notice', text: '本のデータが更新されました。'
+    assert_text 'Ruby超入門2'
+    assert_text '最新版になりました。'
+    assert_text '五十嵐さん2'   
+  end
+
+  test 'Bookの削除' do
+    visit books_url
+    accept_confirm do
+      click_on '削除'
     end
-
-    assert_text "Book was successfully destroyed"
+    assert_selector 'p#notice', text: '本のデータが削除されました。'
   end
 end
