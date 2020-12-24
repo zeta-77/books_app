70fe8aed9e9af9cd6dbb5b8d8d3ce7c73fe1368e
zeta-77 <bts.yuichiro@gmail.com>
2020-12-25 00:52:45 +0900

    不要なコメントを削除

--- test/factories/books.rb
+++ test/factories/books.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :ruby_book, class: Book do
-    # sequence(:id){|i| i}
     title {"Ruby超入門"}
     memo {"わかりやすい"}
     author {"igaigaさん"}
--- test/factories/reports.rb
+++ test/factories/reports.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :report, class: Report do
-    # sequence(:id){|i| i}
     title {'12月10日の日報'}
     content {'テストを実装した。'}
   end
