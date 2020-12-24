ec9f4908cc5b5241899442e60888883728b88533
zeta-77 <bts.yuichiro@gmail.com>
2020-12-25 01:21:19 +0900

    FactoryBotによる不要なユーザ定義を削除して整理

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -2,32 +2,32 @@ require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
   test 'userの一覧表示' do
-    login_user = FactoryBot.create(:login_user)
-    user = FactoryBot.create(:user)
+    login_user = FactoryBot.create(:user)
+    other_user = FactoryBot.create(:user)
     sign_in_as(login_user)    
 
     visit users_url
     assert_selector 'h1', text: 'ユーザ一覧'
-    assert_text user.email
+    assert_text other_user.email
   end
 
   test '特定のユーザをクリック' do
-    login_user = FactoryBot.create(:login_user)
-    user = FactoryBot.create(:user)
+    login_user = FactoryBot.create(:user)
+    other_user = FactoryBot.create(:user)
     sign_in_as(login_user)
     visit users_url
-    click_on user.email
+    click_on other_user.email
 
     assert_selector 'h1', text: 'ユーザ情報'
-    assert_text user.email
+    assert_text other_user.email
   end
 
   test '特定のユーザをフォロー' do
-    login_user = FactoryBot.create(:login_user)
-    user = FactoryBot.create(:user)
+    login_user = FactoryBot.create(:user)
+    followed_user = FactoryBot.create(:user)
     sign_in_as(login_user)
     visit users_url
-    click_on user.email
+    click_on followed_user.email
     click_on 'フォローする'
 
     assert_selector 'h1', text: 'ユーザ一覧'
@@ -35,7 +35,7 @@ class UsersTest < ApplicationSystemTestCase
   end
 
   test '特定のユーザがフォローしているユーザの一覧' do
-    login_user = FactoryBot.create(:login_user)
+    login_user = FactoryBot.create(:user)
     following_user = FactoryBot.create(:user)
     followed_user = FactoryBot.create(:user)
     sign_in_as(login_user)
